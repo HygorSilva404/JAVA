c258e568fc952ec807b65df65613c817071348c8
Hygor Silva <hygorsilva404@gmail.com>
2026-04-04 20:20:57 -0300

    Começo do estudo de condições

---
+++ Aulas/Condições/src/cond.java
@@ -0,0 +1,12 @@
+public class cond {
+    public static void main(String[] args) throws Exception {
+        int idade = 56;
+        if (idade == 18) {
+            System.out.println("Você é um jovem adulto");
+        } else if (idade >= 30 ) {
+            System.out.println("Você já é um adulto formado");
+        } else {
+            System.out.println("Você é menor de idade");
+        }
+ }
+}
